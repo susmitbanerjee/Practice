08830df047fb7d5b9ca1f130c9c9c16bbdd62e00
Susmit Banerjee <susmit.b1@gmail.com>
2023-12-30 21:09:35 +0530

    program for the day

--- NeetCodeRoadMap/ValidParentheses.java
+++ NeetCodeRoadMap/ValidParentheses.java
@@ -8,7 +8,9 @@ public class ValidParentheses {
 
     public static boolean isValid(String s) {
         if (s.length() % 2 != 0) return false;
+
         Stack<Character> stack = new Stack<>();
+
         for (int i = 0; i < s.length(); i++) {
             if (stack.isEmpty() && (s.charAt(i) == ')' || s.charAt(i) == '}' || s.charAt(i) == ']'))
                 return false;
