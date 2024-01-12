ead1f0f4a2f876497428733a28040dafcf4af0ba
Susmit Banerjee <susmit.b1@gmail.com>
2024-01-12 09:57:48 +0530

    program for the day

---
+++ NeetCodeRoadMap/Search2DMatrix.py
@@ -0,0 +1,36 @@
+from typing import List
+
+
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        ROWS, COLS = len(matrix), len(matrix[0])
+        top, bot = 0, ROWS - 1
+        while top <= bot:
+            row = (top + bot) // 2
+            if target > matrix[row][-1]:
+                top = row + 1
+            elif target < matrix[row][0]:
+                bot = row - 1
+            else:
+                break
+        while top >= bot:
+            return False
+        row = (top + bot) // 2
+        l, r = 0, COLS - 1
+        while l <= r:
+            m = (l + r) // 2
+            if target > matrix[row][m]:
+                l = m + 1
+            elif target < matrix[row][m]:
+                r = m - 1
+            else:
+                return True;
+        return False;
+
+
+sc = Solution()
+matrix = [[1, 3, 5, 7],
+          [10, 11, 16, 20],
+          [23, 30, 34, 60]]
+target = 3
+print(sc.searchMatrix(matrix, target))
