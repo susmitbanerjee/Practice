9f02bcde1d6d78a922b100feceb000c9670e7225
Susmit Banerjee <susmit.b1@gmail.com>
2024-04-26 11:58:03 +0530

    commit

--- NeetCodeRoadMap/MaxSteal.java
+++ NeetCodeRoadMap/MaxSteal.java
@@ -39,10 +39,33 @@ public class MaxSteal {
         return stack.isEmpty();
     }
 
+    static int trapRainWater(int[] arr) {
+        int[] left = new int[arr.length];
+        int[] right = new int[arr.length];
+        int total = 0;
+
+        int max = arr[0];
+        for (int i = 0; i < left.length; i++) {
+            left[i] = Math.max(arr[i], max);
+            max = left[i];
+        }
+        max = arr[arr.length - 1];
+        for (int i = arr.length - 1; i >= 0; i--) {
+            right[i] = Math.max(arr[i], max);
+            max = right[i];
+        }
+        for (int i = 0; i < arr.length; i++) {
+            total += (Math.min(left[i], right[i])) - arr[i];
+        }
+        return total;
+    }
+
     public static void main(String[] args) {
         int[] arr = new int[]{2, 21, 4, 7, 17, 35};
         System.out.println(maxSteal(arr));
         String s = "()[}{}";
         System.out.println(validParentheses(s));
+        int[] arr1 = new int[]{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};
+        System.out.println(trapRainWater(arr1));
     }
 }
